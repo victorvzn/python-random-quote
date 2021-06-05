0644ca4533f3a0a12adcdcc36563c2b291d8c5fc
Victor Villazón <vvillazon@itereo.travel>
2021-06-05 03:48:00 -0500

    Learn about file writing and add quotes programmatically

--- get-quote.py
+++ get-quote.py
@@ -4,7 +4,7 @@ def primary():
   # print("Keep it logically awesome.")
 
   f = open("quotes.txt")
-  quotes = f.readlines()
+  quotes = f.readlines() 
   f.close()
   
   last = len(quotes) - 1
@@ -12,8 +12,24 @@ def primary():
   rnd1 = random.randint(0, last)
   rnd2 = random.randint(0, last) 
 
-  print(quotes[rnd1], end="")
-  print(quotes[rnd2], end="")
+  print("RANDOM QUOTE: ", quotes[rnd1], end="")
+  print("RANDOM QUOTE: ", quotes[rnd2], end="")
+
+  new_quote = input("Enter a new quote: ") 
+
+  f = open("quotes.txt", "a")
+  f.write("\n")
+  f.write(new_quote)
+  f.close()
+
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close
+
+  last = len(quotes) - 1
+
+  print()
+  print("NEW QUOTE:", quotes[last], end="")
 
 if __name__== "__main__":
   primary()
